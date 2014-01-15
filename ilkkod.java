a7f3b52c8d4a6595ba2fc7608cd7427055241b91
Can KÜÇÜK <cankucuk@ubuntu.(none)>
2014-01-15 21:17:08 +0200

    Hello World

--- ilkkod.java
+++ ilkkod.java
@@ -3,3 +3,27 @@ class ilkkod{
 		System.out.println("Hello World");
 	}
 }
+/* class ilkkod--- Nesne yönelimli programlamanın yapı taşıdır.
+Her class'ın kendin özgü bir ismi olmalıdır.Burda bizim class'ımızın adı ilkkod'dur.
+Ayrıca daha önce yazılmış class'lar import deyimiyle başka bir programda çağrılabilirler.
+
+;---;(noktalı virgül) javada komutun bittiğini gösterir.
+
+public static void main(String[] args)---Main(ana program) adında bir metoddur bu.
+Program çalışmaya main metodundan başlar.
+Public : Bu metoda dışarıdan erişebilme özelliği katmıştır.
+Void : Void ise hiçbir çıktı alınmayacağını belirtmiştir.Yani bir değer döndürülmeyecektir.
+
+System.out.println("Hello World");----Systen.out metodunun println metodunu çağırır.
+Bu metod metod girişinde gönderilen String tipindeki değişkeni konsol çıktısına aktarır.
+Ayrıca System.out.prinf(); , System.out.print(); metodlarıda vardır.
+System.out.prinln()-- stringi yazar ve alt satıra geçer
+System.out.print()-- Stringi yazar ve o satırda kalır.
+System.out.prinf()-- formatlı yazdırma biçimidir.
+Bir başka konu ise kontrol terimleri vardır.Bunlar;
+\n alt satıra geç
+\t bir sonraki tab’a atla anlamındadır.
+System.out.printf("Hello Word\n");
+*/
+
+
